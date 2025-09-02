6df5756eb90006f4fc3b5f18490d7d2af1fb18d8
Jesus Muller <jesus.muller@softtek.com>
2025-09-02 16:55:09 -0300

    change title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-    <header className="bg-blue-800 text-white p-5">Tipos de renderizado en Next.js</header>
+    <header className="bg-blue-800 text-white p-5">Ejemplos de renderizado en Next.js</header>
     <menu className="flex flex-col items-center mt-3">
       <ul>
         <li className="w-50 bg-gray-200 p-2 text-center rounded hover:bg-gray-400">
